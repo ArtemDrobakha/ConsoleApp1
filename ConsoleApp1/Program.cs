5a4bcb73201b637c8d00c3594b71aa380d87cbe1
ArtemDrobakha <65715806+ArtemDrobakha@users.noreply.github.com>
2021-04-07 01:02:14 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Mytimer
+        {
+            delegate int IntOperation(int i, int j);
+            static IntOperation op;
+            static bool a = true;
+            public static int Sum(int x, int y)
+            {
+                return x + y;
+            }
+
+            public static int Prz(int x, int y)
+            {
+                return x * y;
+            }
+            public static void TimerCallback(Object o)
+            {
+                if (a==true)
+                {
+                    op = Sum;
+                }
+                else
+                {
+                    op = Prz;
+                }
+                Console.WriteLine("Результат: " + op(3,3));
+                a = !a;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int t;
+            Console.WriteLine("t=");
+            t = Convert.ToInt32(Console.ReadLine());
+            Timer timer = new Timer(Mytimer.TimerCallback, null, 0, t);
+            Console.ReadKey();
+        }
+    }
+}
